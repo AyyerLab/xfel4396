59863f0b4dc5cc80a23cc5fa13c12b772ff7ddea
Kartik Ayyer <kartik.ayyer@desy.de>
2023-09-13 10:10:40 +0200

    constants: Updated beamtime constants

---
+++ constants.py
@@ -0,0 +1,13 @@
+'''Constants to be used during beamtime'''
+PREFIX = '/gpfs/exfel/exp/SQS/202302/p004396/scratch/'
+
+DET_FGLOB = 'RAW-R*-PNCCD01-S*.h5'
+DET_DSET = 'INSTRUMENT/SQS_NQS_PNCCD1MP/CAL/PNCCD_FMT-0:output/data/image'
+TRAINID_DSET = 'INDEX/trainId'
+
+DET_META_FGLOB = 'RAW-R*-PNCCD02-S*.h5'
+GAIN_DSET = '/CONTROL/SQS_NQS_PNCCD1MP/MDL/DAQ_GAIN/pNCCDGain/value'
+MOTOR_TYPES = ['UP', 'DOWN', 'LEFT', 'RIGHT', 'FEL']
+MOTOR_DSETS = ['/CONTROL/SQS_NQS_PNCCD/MOTOR/PNCCD_%s/actualPosition/value'%mtype for mtype in MOTOR_TYPES]
+
+ADU_PER_KEV = 4520 # in Gain mode 1
